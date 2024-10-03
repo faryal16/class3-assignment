6638707a727258b8d4cf111425ad6961b81607cf
faryal16 <bintefarzana1992@gmail.com>
2024-10-03 21:12:48 +0500

    file uploaded

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+"use client"
+import Image from "next/image";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+
+export default function Home() {
+  return (
+    <div>
+
+< HeroSection/>
+
+
+    </div>
+  );
+}
+
+
